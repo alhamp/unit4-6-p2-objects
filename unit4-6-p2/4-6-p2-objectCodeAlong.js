8e8223548e703f18366ef3aa941511d1b6b6eeff
Amber Hampton <37483133+alhamp@users.noreply.github.com>
2021-04-03 02:36:29 -0400

    The starter challenge
    Unit 4 Period 2 Objects - Product Profile

---
+++ unit4-6-p2/4-6-p2-objectCodeAlong.js
@@ -0,0 +1,95 @@
+//Code Along Part One
+
+//Initialize Constructors and More!
+//Step 1
+
+
+
+
+//Step 2
+
+
+
+
+//Step 3
+
+
+
+//Step 4
+
+
+//Step 5
+
+
+//Step 6
+
+
+// Step 7
+
+
+// Step 8
+
+
+//.Create
+
+//Step 1
+
+
+//Step 2
+
+
+//Step 3
+
+
+
+//Code Along Part 2
+
+//Array vs Objects
+
+//Objects: Create a person object with properties with name, favorite number and boolean.
+
+//Use dot notation to return favorite number
+
+//Use bracket notation to return name
+
+
+//Arrays: Create an array with 4 different flavors
+
+
+//access third item in the array
+
+//Objects
+//Change the value of the boolean of the object we created.
+
+//Add a new property to our person object such as hobbies with multiple values in the property.
+
+//remove the favorite number property from the person object
+
+
+//Arrays
+//Use push to add another flavor to your array of flavors
+
+
+//use pop to remove the last item from an flavor array
+
+
+//use unshift to add more values to the beginning of the flavor array.
+
+// us shift to remove the item from the flavor of the array
+
+//Object
+//Write a for in loop for the person object you created
+
+//Write a for each loop for your person Object
+
+
+//Arrays
+//Write a for loop for your flavors array
+
+//Write a for of loop for your flavors array
+
+//write a for each loop for your flavors array
+
+
+
+
